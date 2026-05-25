27b9bfb8de2db62c4c43b77b2a4f1b0091e19aa1
Garvit Adlakha <garvitadlakha08@gmail.com>
2026-05-25 23:44:03 +0530

    initial commit: add StoraXApplication class with main method

---
+++ StoraXApplication.java
@@ -0,0 +1,13 @@
+package com.garvitadlakha.storax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoraXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoraXApplication.class, args);
+    }
+
+}
