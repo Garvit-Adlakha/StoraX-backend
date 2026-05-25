65415923bc238a785045adb13155075210615161
Garvit Adlakha <garvitadlakha08@gmail.com>
2026-05-25 23:48:52 +0530

    Initialize StoraX project with Maven setup, application structure, and basic configurations

---
+++ src/test/java/com/garvitadlakha/storax/StoraXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garvitadlakha.storax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoraXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
